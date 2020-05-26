fc689c65ca77380b84d6874ab0d989e0df3fbc8b
rgasch <rgasch@gmail.com>
2020-05-26 23:59:31 +0200

    very minor optimizations + improved comments

--- basicCS/sort11Numbers.php
+++ basicCS/sort11Numbers.php
@@ -1,7 +1,7 @@
 <?php
 
 // Generate 11 random numbers between 0 and 100
-// This method of generating the random values is certainly not optimal, 
+// This method of generating the random values is certainly not optimal,
 // but for the purpose of this excercise, we
 //
 // 1) Know that we're dealing with a small range of possible values
@@ -9,17 +9,17 @@
 //
 //
 //
-// Performance (on my machine, which has a lot of RAM (irrelevant for this excercise) 
-// but is relatively slow in CPU terms (timings are for 1 million interations): 
+// Performance (on my machine, which has a lot of RAM (irrelevant for this excercise)
+// but is nothing special in CPU terms (timings are for 1 million interations):
 //
 // Count Sort: 18.629620075226 secs
 // PHP Native Sort: 0.88299703598022 secs
 //
 //
-// Performance estimate for 10 Billion iterations): 
+// Performance estimate for 10 Billion iterations):
 //
-// Count Sort: 186296 secs = 51.7 hours 
-// PHP Native Sort: 8829 secs = 2.45 hours
+// Count Sort: 18.6296 * 10000 = 1186296 secs = 51.7 hours
+// PHP Native Sort: 0.8829 * 10000 = 8829 secs = 2.45 hours
 //
 
 
@@ -32,30 +32,46 @@ function generateUniqueRandom(int $min, int $max, int $quantity) : array
 }
 
 
-// Tested various sorts, this is the best I was able to come up with, native PHP sort 
-// still beats this by more than an order of magnitude, which makes sense since it 
-// is implemented in native C vs. PHP code.
+// Tested various sorts, this is the best I was able to come up with. This algorithm (counting
+// sort) is highly dependent on the range of values we're trying to sort; for this case it's
+// a really good candidate because we know our range is [0-100].
+//
+// Native PHP sort still beats this by more than an order of magnitude,
+// which makes sense since
+// 1) We're sorting a small (11-item) array
+// 2) Native PHP sort is implemented in compiled C vs. PHP code.
+//
+// #1 means that the penalty we're 'paying' for the (average) O(n log2 n) performance
+// for quicksort is small due to the small number of elements.
 function countSort(array $values) : array
 {
 	$counts = array();
 	$min    = PHP_INT_MAX;
 	$max    = PHP_INT_MIN;
 	foreach ($values as $v) {
-		$counts[$v] = isset($counts[$v]) ? $counts[$v] + 1 : 1;
+	    // No need to check for duplicate entries, the method we use to generate the
+        // random number array guarantees to deliver a unique set of values.
+		$counts[$v] = 1;
 		if ($v < $min) {
 			$min = $v;
-		}
-		if ($v > $max) {
+		} elseif ($v > $max) {
 			$max = $v;
 		}
 	}
 
+	// An alternative way of doing this would be to extract a sorted set of array keys
+    // of $count and iterate over those. However, this sort would also cost CPU cycles
+    // and given that we're only sorting numbers between the range [0-100], the current
+    // approach is fine.
+    // The consideration of which approach to use depends on the size of the array to
+    // sort and the range of possible values. For this case, we're good.
+
 	$sorted = array();
 	for ($i=$min; $i<=$max; $i++) {
 		if (isset($counts[$i])) {
-			for ($j=0; $j<$counts[$i]; $j++) {
-				$sorted[] = $i;
-        		}
+		    // No need to do a loop to account for multiple/identical entries, our
+            // input is guaranteed to consist of unique integers
+            $sorted[] = $i;
 		}
 	}
 
@@ -76,6 +92,7 @@ $values = generateUniqueRandom($min, $max, $count);
 $nRuns  = 1000000;
 
 
+// Self-implemented (PHP-based) sort
 $starttime = microtime(true);
 for ($i=0; $i<$nRuns; $i++) {
 	$sorted = countSort($values);
@@ -84,6 +101,7 @@ $endtime = microtime(true);
 $diff1 = $endtime - $starttime;
 
 
+// PHP Native Sort for comparison
 $starttime = microtime(true);
 for ($i=0; $i<$nRuns; $i++) {
 	$v = $values;
@@ -93,7 +111,7 @@ $endtime = microtime(true);
 $diff2 = $endtime - $starttime;
 
 
-print "------- RESULTS ----------\n";
+print "------- Performance Timings ----------\n";
 print "Count Sort: $diff1 secs\n";
 print "PHP Native Sort: $diff2 secs\n";
 
