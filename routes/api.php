86fdb18fbccbe790bb9972abc4a2ccb9c69d5ab2
rgasch <rgasch@gmail.com>
2020-05-28 11:32:25 +0200

    adding api routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('users', 'ApiController@users');
+Route::get('users/{uid}', 'ApiController@user');
+Route::get('users/{uid}/posts', 'ApiController@userPosts');
+Route::get('posts/{pid}/comments', 'ApiController@userPostComments');
