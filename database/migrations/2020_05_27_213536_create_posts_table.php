15886ecc6dd5732fcbc06df5a03c9da07fb99bc1
rgasch <rgasch@gmail.com>
2020-05-29 00:11:19 +0200

    added index for performance

--- database/migrations/2020_05_27_213536_create_posts_table.php
+++ database/migrations/2020_05_27_213536_create_posts_table.php
@@ -20,6 +20,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->text('body');
             $table->timestamps();
+            $table->index('user_id');
             $table->foreign('user_id')->references('id')->on('users');
         });
 
