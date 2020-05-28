ffdcee0ee4e4942094d979ed832c8b140a8c5d9e
rgasch <rgasch@gmail.com>
2020-05-28 11:32:57 +0200

    adding carthook config file

---
+++ config/carthook.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'apiUrl'        => 'https://jsonplaceholder.typicode.com',
+    'maxCacheItems' => [
+        'users'    => 10,
+        'posts'    => 50,
+        'comments' => 50
+    ]
+
+];
